afbaa756fd1686c590ba64324ba59d8ceaec54fd
dasom <dasom589@gmail.com>
2024-01-12 10:22:18 +0900

    2024-01-12 1교시

---
+++ src/com/javaex/ex01/MyListApp.java
@@ -0,0 +1,24 @@
+package com.javaex.ex01;
+
+public class MyListApp {
+	
+	public static void main(String[] args) {
+		
+		/*
+		Point[] pArray=new Point[3];	//개수를 확정해야 한다
+		Point p1=new Point(2,2);
+		Point p2=new Point(12,13);
+		Point p3=new Point(22,23);
+		
+		pArray[0]=p1;
+		pArray[1]=p2;
+		pArray[2]=p3;
+		
+		for (int i=0; i<pArray.length; i++) {
+			System.out.println(pArray[i]);
+		}
+		*/
+		
+	}
+
+}
